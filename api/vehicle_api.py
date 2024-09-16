d84d318ffadfd7ef4127694eccf3886c7c5e8362
RossokhinDO <daniil.rossokhin@vitech.team>
2024-09-16 17:47:45 +0300

    Поправил тип вложения

--- api/vehicle_api.py
+++ api/vehicle_api.py
@@ -1,4 +1,5 @@
 import allure
+from allure_commons.types import AttachmentType
 
 from api import routes
 from api.models.getVehicle_models import GetVehicleRequestSchema
@@ -6,7 +7,7 @@ from api.models.getVehicle_models import GetVehicleRequestSchema
 
 @allure.step("Отправить GET запрос на ручку /get_vehicle")
 def get_vehicle(client, body: GetVehicleRequestSchema):
-    allure.attach(body.json(), name="Запрос", attachment_type="JSON")
+    allure.attach(body.json(), name="Запрос", attachment_type=AttachmentType.JSON)
     response = client.post(routes.Routes.GET_VEHICLE, json=body.dict())
-    allure.attach(response.content, name="Ответ", attachment_type="JSON")
+    allure.attach(response.content, name="Ответ", attachment_type=AttachmentType.JSON)
     return response
