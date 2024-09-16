bbd0f4467dd7211e86b67cfbda2348f3201e6160
RossokhinDO <daniil.rossokhin@vitech.team>
2024-09-16 17:20:15 +0300

    Добавил структуру плюс добавил логирование ответа

--- api/vehicle_api.py
+++ api/vehicle_api.py
@@ -6,7 +6,7 @@ from api.models.getVehicle_models import GetVehicleRequestSchema
 
 @allure.step("Отправить GET запрос на ручку /get_vehicle")
 def get_vehicle(client, body: GetVehicleRequestSchema):
-    allure.attach(body, name="Запрос")
+    allure.attach(body.json(), name="Запрос", attachment_type="JSON")
     response = client.post(routes.Routes.GET_VEHICLE, json=body.dict())
-    allure.attach(response, name="Ответ")
+    allure.attach(response.content, name="Ответ", attachment_type="JSON")
     return response
