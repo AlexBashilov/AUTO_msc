0dc8fe0a4195b027c04104980a9c01f5038bfd19
RossokhinDO <daniil.rossokhin@vitech.team>
2024-10-09 16:19:29 +0300

    feature/LOGAUTO-641 Добавил фикстуру для фейкера

--- testData/transport_companies.py
+++ testData/transport_companies.py
@@ -23,23 +23,21 @@ class TransportCompanies:
     PHONE_NUMBER = 'Номер телефона'
     EMAIL = 'Почта контактного лица'
 
-    def generate_random_tc(self, case_name):
-        fake = Faker()
-
+    def generate_random_tc(self, case_name, faker):
         tc = {
-            self.FULL_NAME_OF_THE_TRANSPORTER: 'ТК для кейса ' + case_name + str(fake.random_number(digits=3)),
-            self.REGISTERED_ADDRESS_TRANSPORTER: fake.address(),
-            self.SHORT_NAME_TRANSPORTER: 'ТК для кейса ' + str(fake.random_number(digits=3)),
+            self.FULL_NAME_OF_THE_TRANSPORTER: 'ТК для кейса ' + case_name + str(faker.random_number(digits=3)),
+            self.REGISTERED_ADDRESS_TRANSPORTER: faker.address(),
+            self.SHORT_NAME_TRANSPORTER: 'ТК для кейса ' + str(faker.random_number(digits=3)),
             self.OWNERSHIP_FORM: self.OWNERSHIP_FORM_TYPE_OOO,
-            self.OGRN_TRANSPORTER: fake.random_number(digits=15),
-            self.KPP_TRANSPORTER: fake.random_number(digits=9, fix_len=True),
-            self.INN_TRANSPORTER: fake.random_number(digits=12),
-            self.CONTRACT_NUMBER: fake.random_number(digits=20),
+            self.OGRN_TRANSPORTER: faker.random_number(digits=15),
+            self.KPP_TRANSPORTER: faker.random_number(digits=9, fix_len=True),
+            self.INN_TRANSPORTER: faker.random_number(digits=12),
+            self.CONTRACT_NUMBER: faker.random_number(digits=20),
             # self.CONTRACT_DATE: fake.date(pattern='%d-%m-%Y'),
             self.CONTRACT_DATE: '09012024',
-            self.CONTACT_FACE_TRANSPORTER: fake.name(),
-            self.POSITION_OF_CONTACT_PERSON: fake.job(),
-            self.PHONE_NUMBER: fake.phone_number(),
-            self.EMAIL: fake.email()
+            self.CONTACT_FACE_TRANSPORTER: faker.name(),
+            self.POSITION_OF_CONTACT_PERSON: faker.job(),
+            self.PHONE_NUMBER: faker.phone_number(),
+            self.EMAIL: faker.email()
         }
         return tc
