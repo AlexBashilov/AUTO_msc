8a24e59314c2393ee95c7919feb25bed28d95548
RossokhinDO <daniil.rossokhin@vitech.team>
2024-12-09 19:01:38 +0700

    LOGAUTO-642: Правки по линтеру

--- test_data/test_params/invalid_routes.py
+++ test_data/test_params/invalid_routes.py
@@ -32,105 +32,101 @@ class InvalidRoutes:
                     },
                 ],
             },
-            # {
-            #     'name': 'одной точкой загрузки и двумя точками разгрузки',
-            # 'allureID': '27548',
-            # 'expectedError': ErrorRoutesMessages.DIFFERENT_OPERATION_ERROR,
-            # 'routeDistrict': FederalDistrict.CFO_DISTRICT,
-            # 'routePointOperation':
-            # [
-            #     {
-            #         'pointName': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # 'operationType': RouteOperationType.LOADING,
-            # 'unloadPoint': None,
-            # },
-            # {
-            #     'pointName': ShopList.NN_SUZDAL_SHOP_WAREHOUSE,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # },
-            # {
-            #     'pointName': ShopList.KAZAN_WAREHOUSE,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # }
-            # ],
-            # },
-            # {
-            #     'name': 'несвязными операциями (есть 2 загрузки, но используется только 1)',
-            # 'allureID': '40871',
-            # 'expectedError': ErrorRoutesMessages.UNRELATED_OPERATION_ERROR,
-            # 'routeDistrict': FederalDistrict.CFO_DISTRICT,
-            # 'routePointOperation':
-            # [
-            #     {
-            #         'pointName': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # 'operationType': RouteOperationType.LOADING,
-            # 'unloadPoint': None,
-            # },
-            # {
-            #     'pointName': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # 'operationType': RouteOperationType.LOADING,
-            # 'unloadPoint': None,
-            # },
-            # {
-            #     'pointName': ShopList.NN_SUZDAL_SHOP_WAREHOUSE,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # },
-            # {
-            #     'pointName': ShopList.KAZAN_WAREHOUSE,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # }
-            # ],
-            # },
-            # {
-            #     'name': 'зацикленными операциями (загрузка в одном и том же складе, после разгрузки)',
-            # 'allureID': '40869',
-            # 'expectedError': ErrorRoutesMessages.LOOP_OPERATION_ERROR,
-            # 'routeDistrict': FederalDistrict.CFO_DISTRICT,
-            # 'routePointOperation':
-            # [
-            #     {
-            #         'pointName': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # 'operationType': RouteOperationType.LOADING,
-            # 'unloadPoint': None,
-            # },
-            # {
-            #     'pointName': ShopList.KAZAN_WAREHOUSE,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # },
-            # {
-            #     'pointName': ShopList.KAZAN_WAREHOUSE,
-            # 'operationType': RouteOperationType.LOADING,
-            # 'unloadPoint': None,
-            # },
-            # {
-            #     'pointName': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.KAZAN_WAREHOUSE,
-            # },
-            # ],
-            # },
-            # {
-            #     'name': 'точками без лог.цепочек',
-            # 'allureID': '40897',
-            # 'expectedError': ErrorRoutesMessages.LOG_CHAIN_ERROR,
-            # 'routeDistrict': FederalDistrict.CFO_DISTRICT,
-            # 'routePointOperation':
-            # [
-            #     {
-            #         'pointName': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # 'operationType': RouteOperationType.LOADING,
-            # 'unloadPoint': None,
-            # },
-            # {
-            #     'pointName': ShopList.MRK_ATLANTIC_SHOP,
-            # 'operationType': RouteOperationType.UNLOADING,
-            # 'unloadPoint': ShopList.MSK_DOMODEDOVO_WAREHOUSE,
-            # },
-            # ],
-            # },
+            {
+                "name": "одной точкой загрузки и двумя точками разгрузки",
+                "allureID": "27548",
+                "expectedError": ErrorRoutesMessages.DIFFERENT_OPERATION_ERROR,
+                "routeDistrict": FederalDistrict.CFO_DISTRICT,
+                "routePointOperation": [
+                    {
+                        "pointName": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                        "operationType": RouteOperationType.LOADING,
+                        "unloadPoint": None,
+                    },
+                    {
+                        "pointName": ShopList.NN_DOSKINO_WAREHOUSE,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                    },
+                    {
+                        "pointName": ShopList.KAZAN_WAREHOUSE,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                    },
+                ],
+            },
+            {
+                "name": "несвязными операциями (есть 2 загрузки, но используется только 1)",
+                "allureID": "40871",
+                "expectedError": ErrorRoutesMessages.UNRELATED_OPERATION_ERROR,
+                "routeDistrict": FederalDistrict.CFO_DISTRICT,
+                "routePointOperation": [
+                    {
+                        "pointName": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                        "operationType": RouteOperationType.LOADING,
+                        "unloadPoint": None,
+                    },
+                    {
+                        "pointName": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                        "operationType": RouteOperationType.LOADING,
+                        "unloadPoint": None,
+                    },
+                    {
+                        "pointName": ShopList.NN_DOSKINO_WAREHOUSE,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                    },
+                    {
+                        "pointName": ShopList.KAZAN_WAREHOUSE,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                    },
+                ],
+            },
+            {
+                "name": "зацикленными операциями (загрузка в одном и том же складе, после разгрузки)",
+                "allureID": "40869",
+                "expectedError": ErrorRoutesMessages.LOOP_OPERATION_ERROR,
+                "routeDistrict": FederalDistrict.CFO_DISTRICT,
+                "routePointOperation": [
+                    {
+                        "pointName": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                        "operationType": RouteOperationType.LOADING,
+                        "unloadPoint": None,
+                    },
+                    {
+                        "pointName": ShopList.KAZAN_WAREHOUSE,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                    },
+                    {
+                        "pointName": ShopList.KAZAN_WAREHOUSE,
+                        "operationType": RouteOperationType.LOADING,
+                        "unloadPoint": None,
+                    },
+                    {
+                        "pointName": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.KAZAN_WAREHOUSE,
+                    },
+                ],
+            },
+            {
+                "name": "точками без лог.цепочек",
+                "allureID": "40897",
+                "expectedError": ErrorRoutesMessages.LOG_CHAIN_ERROR,
+                "routeDistrict": FederalDistrict.CFO_DISTRICT,
+                "routePointOperation": [
+                    {
+                        "pointName": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                        "operationType": RouteOperationType.LOADING,
+                        "unloadPoint": None,
+                    },
+                    {
+                        "pointName": ShopList.MRK_ATLANTIC_SHOP,
+                        "operationType": RouteOperationType.UNLOADING,
+                        "unloadPoint": ShopList.MSK_DOMODEDOVO_WAREHOUSE,
+                    },
+                ],
+            },
         ]
